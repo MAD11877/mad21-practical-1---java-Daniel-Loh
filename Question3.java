f4172b646592bbf1c963beaff7a4e2c9e7a6fdb2
Daniel-Loh <82802546+Daniel-Loh@users.noreply.github.com>
2021-04-21 23:14:46 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,8 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int val = in.nextInt();
+    int result = val * val;
+    System.out.println(result);
   }
 }
