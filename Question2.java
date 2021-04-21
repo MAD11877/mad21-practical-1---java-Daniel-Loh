e8189ecb55231e1e7ff4e7069a12c1233394d3c6
Daniel-Loh <82802546+Daniel-Loh@users.noreply.github.com>
2021-04-21 22:40:54 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,12 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter Height in Meters: ");
     double height = in.nextDouble();
+
+    System.out.println("Enter Weight: ");
+    double weight = in.nextDouble();
+    double BMI = weight / (height * height);
+    System.out.println("Your BMI is " + BMI);
   }
 }
